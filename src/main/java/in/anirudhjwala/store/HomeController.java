05658afe56b5ed93114a3f8963e6802d9579ed36
Anirudh Jwala <anirudhsai2@gmail.com>
2025-04-03 02:45:42 +0530

    setting and getting application properties

--- src/main/java/in/anirudhjwala/store/HomeController.java
+++ src/main/java/in/anirudhjwala/store/HomeController.java
@@ -1,12 +1,18 @@
 package in.anirudhjwala.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    // Reading values from application.properties
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
+        System.out.println("Running appName: " + appName);
         return "index.html";
     }
 }
