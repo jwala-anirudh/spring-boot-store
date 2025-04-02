202f2a7c46fb0d2064c548d4698ff8154d7b3641
Anirudh Jwala <anirudhsai2@gmail.com>
2025-04-03 01:34:08 +0530

    boilerplate code of spring boot store application

---
+++ src/test/java/in/anirudhjwala/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.anirudhjwala.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
