98e9396e6c2950515eb18ae16ed5aa6343f09e10
Anirudh Jwala <anirudhsai2@gmail.com>
2025-04-03 12:21:02 +0530

    constructor injection

--- src/main/java/in/anirudhjwala/store/OrderService.java
+++ src/main/java/in/anirudhjwala/store/OrderService.java
@@ -1,9 +1,17 @@
 package in.anirudhjwala.store;
 
 public class OrderService {
+    private PaymentService paymentService;
+
+    // Constructor injection
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
-        var paymentService = new StripePaymentService();
+        // var paymentService = new StripePaymentService();
+
+        // Use the value set in constructor in-place of StripePaymentService
         paymentService.processPayment(10);
     }
 
